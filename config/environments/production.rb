2dd7ff59ab53bf2ba5583e1a9c84efff61f7b3e8
Konomu_Ito <konomu1117@gmail.com>
2020-07-17 09:37:49 +0000

    Add password reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
   config.force_ssl = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://fatidique-gerard-56060.herokuapp.com'
+  host = 'https://rails-sample2020.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
